1988a04b6595d7720d69cf760bf5a68d37a6b0d5
나선재 <sky114z21@gmail.com>
2024-05-01 16:53:09 +0900

    코드 수정

--- build-docker.sh
+++ build-docker.sh
@@ -2,14 +2,14 @@
 
 GIT_BRANCH=""
 
-if [ -n "$BRANCH_NAME" ]; then
-  GIT_BRANCH=$BRANCH_NAME
-else
-  GIT_BRANCH=$(git symbolic-ref --short HEAD)
-fi
-
 gradle wrapper
 
 PROJECT_VER=$(./gradlew properties | grep -Po '(?<=version: ).*')
 
-docker buildx build --platform linux/amd64 -t teamup.server:$PROJECT_VER-$GIT_BRANCH .
+if [ -n "$BRANCH_NAME" ]; then
+  GIT_BRANCH=$BRANCH_NAME
+  docker buildx build --platform linux/amd64 -t teamup.server:$PROJECT_VER-$GIT_BRANCH .
+else
+  GIT_BRANCH=$(git symbolic-ref --short HEAD)
+  docker build -t teamup.server:$PROJECT_VER-$GIT_BRANCH .
+fi
