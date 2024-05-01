1ab093b430c9877ee51360697aae4c2069a42aa5
나선재 <sky114z21@gmail.com>
2024-05-02 00:52:17 +0900

    인증코드 테이블 제거

--- src/main/java/com/sjna/teamup/repository/UserVerificationCodeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sjna.teamup.repository;
-
-import com.sjna.teamup.entity.UserVerificationCode;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserVerificationCodeRepository extends JpaRepository<UserVerificationCode, Long> {
-}
